f88f53fd2c3a322bad964078b74fa652a0cdb9cf
ZerglingGo <zerglinggo@zerglinggo.net>
2023-08-21 15:36:56 +0900

    Update scripts

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
-sudo docker-compose --env-file ./app/.env build
+docker compose build
 
 if [ $? -eq 0 ]; then
     echo -e "\n=======================================\n\e[1;32m\tCompleted build laravel\e[0m\n=======================================\n"
 
     echo -e 'You can running server to execute command below:'
-    echo -e '    docker-compose --env-file ./app/.env up'
+    echo -e '    docker compose up'
 fi
--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ if [ -d "app" ]; then
     exit 1
 fi
 
-sudo docker run --rm -it \
+docker run --rm -it \
     --volume "$(pwd):/app" \
     --user $(id -u):$(id -g) \
     composer:2 \
@@ -24,9 +24,9 @@ sudo docker run --rm -it \
             --classmap-authoritative
     "
 
-sudo chmod -R 777 app/bootstrap/cache app/storage
+chmod -R 777 app/bootstrap/cache app/storage
 
-sudo docker run --rm -it \
+docker run --rm -it \
     --volume "$(pwd):/app" \
     --user $(id -u):$(id -g) \
     --workdir "/app/app" \
@@ -43,11 +43,11 @@ if [ $? -eq 0 ]; then
     echo -e "\n============================================\n\e[1;32m\tCompleted initialize Laravel\e[0m\n============================================\n"
 
     echo -e '======== \e[1;33mUseful aliases\e[0m ========'
-    echo -e "alias ld-artisan='sudo docker-compose --env-file=app/.env run php php artisan'"
-    echo -e "alias ld-composer='sudo docker run --rm -it --volume \"$(pwd):/app\" --user $(id -u):$(id -g) --workdir /app/app composer:2 composer'"
-    echo -e "alias ld-npm='sudo docker run --rm -it --volume \"$(pwd):/app\" --user $(id -u):$(id -g) --workdir /app/app -p=5173:5173 node:lts npm'"
-    echo -e "alias ld-npx='sudo docker run --rm -it --volume \"$(pwd):/app\" --user $(id -u):$(id -g) --workdir /app/app node:lts npx'"
+    echo -e "alias artisan='docker compose run php php artisan'"
+    echo -e "alias composer='docker run --rm -it --volume \"$(pwd):/app\" --user $(id -u):$(id -g) --workdir /app/app composer:2 composer'"
+    echo -e "alias npm='docker run --rm -it --volume \"$(pwd):/app\" --user $(id -u):$(id -g) --workdir /app/app -p=5173:5173 node:lts npm'"
+    echo -e "alias npx='docker run --rm -it --volume \"$(pwd):/app\" --user $(id -u):$(id -g) --workdir /app/app node:lts npx'"
     echo -e '============================================='
 
-    echo -e '\n\e[1;33mPlease edit your ./app/.env file Before running ./build.sh\e[0m'
+    echo -e '\n\e[1;33mPlease edit your .env file Before running ./build.sh\e[0m'
 fi
